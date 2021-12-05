5501380149b5e1789e7605d982aaa3b5ecee0938
mememe653 <95522491+mememe653@users.noreply.github.com>
2021-12-05 16:29:24 +1030

    Get snake moving

--- src/com/mememe653/snake/Window.java
+++ src/com/mememe653/snake/Window.java
@@ -3,10 +3,15 @@ package com.mememe653.snake;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class Window extends JPanel {
+public class Window extends JPanel implements ActionListener {
+	
+	private final int DELAY = 100;
 	
 	private final int WIDTH = 300;
 	private final int HEIGHT = 300;
@@ -25,10 +30,14 @@ public class Window extends JPanel {
 	private int apple_x;
 	private int apple_y;
 	
+	private final Timer timer = new Timer(DELAY, this);
+	
 	public Window() {
 		initWindow();
 		initSnake();
 		initApple();
+		
+		timer.start();
 	}
 	
 	private void initWindow() {
@@ -66,6 +75,21 @@ public class Window extends JPanel {
 		for (int i = 0; i < dots; i++) {
 			g.fillRect(x[i], y[i], DOT_WIDTH, DOT_WIDTH);
 		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		moveSnake();
+		repaint();
+	}
+	
+	private void moveSnake() {
+		for (int i = dots - 1; i > 0; i--) {
+			x[i] = x[i-1];
+			y[i] = y[i-1];
+		}
+		x[0] += DOT_WIDTH;
+		x[0] = x[0] % WIDTH;
 	}
 
 }
