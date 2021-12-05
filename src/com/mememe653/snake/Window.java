85080e394370389c45263af83c74734b5bd06630
mememe653 <95522491+mememe653@users.noreply.github.com>
2021-12-05 16:06:14 +1030

    Draw snake and apple onto window

--- src/com/mememe653/snake/Window.java
+++ src/com/mememe653/snake/Window.java
@@ -11,13 +11,61 @@ public class Window extends JPanel {
 	private final int WIDTH = 300;
 	private final int HEIGHT = 300;
 	
+	private final Color SNAKE_COLOR = Color.green;
+	private final Color APPLE_COLOR = Color.red;
+	
+	private final int DOT_WIDTH = 10;
+	private final int MAX_DOTS = (int) (WIDTH * HEIGHT / Math.pow(DOT_WIDTH, 2));
+	private final int INIT_SNAKE_LENGTH = 5;
+	private int dots = 0;
+	
+	private int x[] = new int[MAX_DOTS];
+	private int y[] = new int[MAX_DOTS];
+	
+	private int apple_x;
+	private int apple_y;
+	
 	public Window() {
 		initWindow();
+		initSnake();
+		initApple();
 	}
 	
 	private void initWindow() {
 		setBackground(Color.black);
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	}
+	
+	private void initSnake() {
+		for (int i = 0; i < INIT_SNAKE_LENGTH; i++) {
+			x[INIT_SNAKE_LENGTH - i - 1] = i * DOT_WIDTH;
+			y[i] = 0;
+			dots++;
+		}
+	}
+	
+	private void initApple() {
+		relocateApple();
+	}
+	
+	private void relocateApple() {
+		apple_x = (int) (Math.random() * WIDTH);
+		apple_y = (int) (Math.random() * HEIGHT);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		// Draw apple
+		g.setColor(APPLE_COLOR);
+		g.fillRect(apple_x, apple_y, DOT_WIDTH, DOT_WIDTH);
+		
+		// Draw snake
+		g.setColor(SNAKE_COLOR);
+		for (int i = 0; i < dots; i++) {
+			g.fillRect(x[i], y[i], DOT_WIDTH, DOT_WIDTH);
+		}
 	}
 
 }
